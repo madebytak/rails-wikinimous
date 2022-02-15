6d3f9dd776bfa83eba8ff21572438f709c711958
Takeshi Shinohara <takeshigs@gmail.com>
2022-02-15 16:47:50 +0900

    working on the nav bar button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,12 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def create; end
+  def new; end
+  def edit; end
+  def show; end
+  def update; end
+  def destory; end
 end
