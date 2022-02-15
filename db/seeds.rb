65ba463fe0e2ccd71e6d03518691f10228111738
Takeshi Shinohara <takeshigs@gmail.com>
2022-02-15 16:17:39 +0900

    working on the git routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating 10 fake articles...'
+10.times do
+  Article.create(
+    title: Faker::Movie.title,
+    content: Faker::Quotes::Shakespeare.as_you_like_it_quote
+  )
+end
